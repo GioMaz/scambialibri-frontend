3c82949466298cfb101e52b2b1529b0f5b6714b6
GioMaz <giovimaz2003@gmail.com>
2021-12-10 17:10:39 +0100

    fixed notification style

--- src/components/Notifications.vue
+++ src/components/Notifications.vue
@@ -1,7 +1,9 @@
 <template>
-  <div v-for="notification in notificationsStore.notifications" :key="notification">
-    <div class="p-4 mx-4 mt-4 lg:w-1/4 bottom-0 right-0 rounded-md text-white" :id="notification.type">
-      {{ notification.value }}
+  <div class="absolute bottom-4 right-0 w-full md:w-1/3">
+    <div v-for="notification in notificationsStore.notifications" :key="notification">
+      <div class="p-4 mx-4 mt-4 rounded-md text-white" :id="notification.type">
+        {{ notification.value }}
+      </div>
     </div>
   </div>
 </template>
